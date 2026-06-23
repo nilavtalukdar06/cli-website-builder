dfcf6e9928b3a3bc2d97f64c4c73c5b9215270e5
Web <webd75369@gmail.com>
2026-06-23 22:09:43 +0530

    minor bug fix

--- apps/server/src/controllers/create-app.ts
+++ apps/server/src/controllers/create-app.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { Agents } from "src/services/agents";
 import { ApiError } from "src/utils/error";
+import { ApiResponse } from "src/utils/response";
 
 export const createApp = async (req: Request, res: Response) => {
   try {
@@ -15,7 +16,9 @@ export const createApp = async (req: Request, res: Response) => {
       );
     }
     const result = await Agents.createApp(prompt);
-    return result;
+    return res.status(StatusCodes.CREATED).json(
+      new ApiResponse(StatusCodes.CREATED, true, "successfully created the application", result),
+    )
   } catch (error) {
     if (error instanceof ApiError) {
       return res
